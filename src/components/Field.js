84e8e2e9a43e5da2fda7de6f21c182a4d84636a5
Joe Abdel Sater <joegabdelsater@hotmail.com>
2021-04-11 19:53:22 +0300

    set form values

--- src/components/Field.js
+++ src/components/Field.js
@@ -3,11 +3,15 @@ import { FaTrashAlt } from 'react-icons/fa';
 import { SchemaContext } from '../contexts/schemaContext';
 
 const Field = (props) => {
-    const {  tableIndex, fieldIndex } = props;
-    const { schema, updateColumnName } = useContext(SchemaContext);
+    const { tableIndex, fieldIndex } = props;
+    const { schema, updateColumnName, updateColumnConfig } = useContext(SchemaContext);
 
     const handleChange = (event) => {
         updateColumnName(event.target.value, tableIndex, fieldIndex)
+    }
+
+    const handleConfig = (config, event) => {
+        updateColumnConfig(config, event.target.value, tableIndex, fieldIndex)
     }
 
     return (
@@ -26,15 +30,30 @@ const Field = (props) => {
             <div>
                 <div>
                     <label className="text-purple-500 mr-3">Migration:</label>
-                    <input type="text" placeholder="Column type" className="placeholder-gray-500 placeholder-opacity-50 focus:outline-none focus:ring-0 focus:border-purple-500 border-0 border-b-2 border-gray-100 flex-grow " />
+                    <input
+                        value={schema[tableIndex].columns[fieldIndex].column.migrations}
+                        onChange={(event) => handleConfig('migrations', event)}
+                        type="text"
+                        placeholder="Column type"
+                        className="placeholder-gray-500 placeholder-opacity-50 focus:outline-none focus:ring-0 focus:border-purple-500 border-0 border-b-2 border-gray-100 flex-grow " />
                 </div>
                 <div>
                     <label className="text-purple-500 mr-3">Validation:</label>
-                    <input type="text" placeholder="Field validation" className="placeholder-gray-500 placeholder-opacity-50 focus:outline-none focus:ring-0 focus:border-purple-500 border-0 border-b-2 border-gray-100 flex-grow " />
+                    <input
+                        value={schema[tableIndex].columns[fieldIndex].column.validations}
+                        onChange={(event) => handleConfig('validations', event)}
+                        type="text"
+                        placeholder="Field validation"
+                        className="placeholder-gray-500 placeholder-opacity-50 focus:outline-none focus:ring-0 focus:border-purple-500 border-0 border-b-2 border-gray-100 flex-grow " />
                 </div>
                 <div>
                     <label className="text-purple-500 mr-3">Backpack:</label>
-                    <input type="text" placeholder="Field in backpack" className="placeholder-gray-500 placeholder-opacity-50 focus:outline-none focus:ring-0 focus:border-purple-500 border-0 border-b-2 border-gray-100 flex-grow " />
+                    <input
+                        value={schema[tableIndex].columns[fieldIndex].column.backpack}
+                        onChange={(event) => handleConfig('backpack', event)}
+                        type="text"
+                        placeholder="Field in backpack"
+                        className="placeholder-gray-500 placeholder-opacity-50 focus:outline-none focus:ring-0 focus:border-purple-500 border-0 border-b-2 border-gray-100 flex-grow" />
                 </div>
             </div>
         </div>
--- src/contexts/schemaContext.js
+++ src/contexts/schemaContext.js
@@ -44,9 +44,9 @@ const reducer = (state, action) => {
                 schemaCopy[action.payload.tableIndex].columns[action.payload.columnIndex].column = action.payload.value;
                 return {...state, schema: schemaCopy}
 
-            case 'update_config':
+            case 'update_column_config':
+                schemaCopy[action.payload.tableIndex].columns[action.payload.columnIndex].config[action.payload.config] = action.payload.value;
                 return {...state, schema: schemaCopy}
-
         default:
             return state;
     }
@@ -75,7 +75,8 @@ export const SchemaContextProvider = (props) => {
             addColumn: (index) => dispatch({ type: 'add_column', payload: index }),
             addTable: () => dispatch({ type: 'add_table' }),
             updateTableName: (value, index) => dispatch({type: 'update_table_name', payload: {value, index}}),
-            updateColumnName: (value, tableIndex, columnIndex) => dispatch({type: 'update_column_name', payload: {value, tableIndex, columnIndex}})
+            updateColumnName: (value, tableIndex, columnIndex) => dispatch({type: 'update_column_name', payload: {value, tableIndex, columnIndex}}),
+            updateColumnConfig: (config, value, tableIndex, columnIndex) => dispatch({type: 'update_column_config', payload: {config, value, tableIndex, columnIndex}}),
         }}
     >
         {props.children}
